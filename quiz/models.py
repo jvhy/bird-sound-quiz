f4087fddf289fe26222abc288f2ab5b2bc7b079f
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-07-18 21:59:48 +0300

    Alter db model constraints

--- quiz/models.py
+++ quiz/models.py
@@ -3,15 +3,16 @@ from django.db import models
 
 class Species(models.Model):
     name_en = models.CharField(max_length=50, verbose_name="English name")
-    name_sci = models.CharField(max_length=50, verbose_name="Scientific (latin) name")
+    name_sci = models.CharField(max_length=50, unique=True, verbose_name="Scientific (latin) name")
 
 
 class Recording(models.Model):
-    species_id = models.ForeignKey(Species, on_delete=models.CASCADE)
+    id = models.IntegerField(primary_key=True, verbose_name="Xeno-Canto ID")
+    species = models.ForeignKey(Species, on_delete=models.CASCADE)
     url = models.CharField(max_length=50, unique=True, verbose_name="Xeno-Canto URL")
     recordist = models.CharField(max_length=100)
     country = models.CharField(max_length=50)
     location = models.CharField(max_length=255)
     sound_type = models.CharField(max_length=100, null=True)
     license = models.CharField(max_length=30, verbose_name="Creative Commons license type")
-    audio = models.FileField(upload_to="audio", unique=True)
+    audio = models.FileField(upload_to="audio", max_length=255, unique=True)
