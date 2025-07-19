1b1e9c46a6e9648237496c2a89c6599c83e3c39e
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-07-19 16:20:58 +0300

    Add base HTML template and basic index page

--- bird_sound_quiz/settings.py
+++ bird_sound_quiz/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'bird_sound_quiz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Welcome to Bird Quiz!")
+    return render(request, 'index.html')
 
 
 def quiz_page(request):
