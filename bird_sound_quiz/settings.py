75ad916838f5d6e775f2f0436a28be53d0023368
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-07-19 15:58:47 +0300

    Read API keys from .env

--- bird_sound_quiz/settings.py
+++ bird_sound_quiz/settings.py
@@ -10,11 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import environ
+import os
 from pathlib import Path
+
+
+env = environ.Env(
+    DEBUG=(bool, True)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -23,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&4=q@00^fd0fc@cirn+^b%mu$*)nis%cudd5#mz-c6x-w^90do'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# API keys, needed for populating db
+LAJIFI_API_TOKEN = env("LAJIFI_API_TOKEN", default=None)
+XENOCANTO_API_KEY = env("XENOCANTO_API_KEY", default=None)
