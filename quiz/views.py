498b09ed5d3649e61300733b9076834a2d45a1a6
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-07-23 16:20:28 +0300

    Add quiz & results pages

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from quiz.models import Recording
+from quiz.services import get_quiz_recordings
 
 
 def index(request):
@@ -7,8 +9,16 @@ def index(request):
 
 
 def quiz_page(request):
-    return HttpResponse("This page will contain the quiz slides.")
+    recordings = get_quiz_recordings(10)
+    return render(request, 'quiz.html', context={"recordings": recordings})
 
 
 def results_page(request):
-    return HttpResponse("This page will contain the quiz results.")
+    recording_ids = request.POST.getlist("ids[]")
+    answers = [a.lower().strip() for a in request.POST.getlist("answers[]")]
+    recordings = Recording.objects.filter(id__in=recording_ids).select_related("species")
+    recordings_by_id = {str(r.id): r for r in recordings}
+    correct_answers = [recordings_by_id[rec_id].species.name_en.lower() for rec_id in recording_ids]
+    results = zip(answers, correct_answers)
+    score = sum(ua == ca.strip().lower() for ua, ca in results)
+    return render(request, 'results.html', context={"score": score})
