c9c059450013f0ca7d573620bf86d71495b45ad8
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-07-26 12:56:05 +0300

    Add taxonomic info to species model

--- quiz/models.py
+++ quiz/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Species(models.Model):
     name_en = models.CharField(max_length=50, verbose_name="English name")
     name_sci = models.CharField(max_length=50, unique=True, verbose_name="Scientific (latin) name")
+    order = models.CharField(max_length=50, verbose_name="Taxonomic order", null=True, default=None)
+    family = models.CharField(max_length=50, verbose_name="Taxonomic family", null=True, default=None)
+    genus = models.CharField(max_length=50, verbose_name="Taxonomic genus", null=True, default=None)
 
 
 class Recording(models.Model):
@@ -19,3 +22,13 @@ class Recording(models.Model):
     license_url = models.CharField(max_length=255, verbose_name="Creative Commons license info URL", null=True)
     audio = models.FileField(upload_to="audio", max_length=255, unique=True)
     downloaded = models.BooleanField(verbose_name="Audio file downloaded", default=False)
+
+
+class Region(models.Model):
+    code = models.CharField(max_length=9)
+    name = models.CharField(max_length=100)
+
+
+class Observation(models.Model):
+    species = models.ForeignKey(Species, on_delete=models.CASCADE)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
