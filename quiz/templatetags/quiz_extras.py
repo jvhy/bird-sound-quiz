23802af3f30856dd69e72bd70c3b9707fe4089ed
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-08-01 14:26:13 +0300

    Add support for multi-choice mode in quiz view

--- quiz/templatetags/quiz_extras.py
+++ quiz/templatetags/quiz_extras.py
@@ -1,4 +1,5 @@
 from operator import attrgetter
+from typing import Any
 
 from django import template
 
@@ -16,3 +17,8 @@ def get_attr(recording: Recording, attr: str):
     :param attr: Attribute to get from Recording object, eg. "xc_audio_url" or "audio.url"
     """
     return attrgetter(attr)(recording)
+ 
+
+@register.filter
+def dict_get(dictionary: dict, key: Any):
+    return dictionary[key]
