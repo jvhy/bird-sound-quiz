6d642e5445aea04e4b5026c358ca28157e041994
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-08-06 09:42:22 +0300

    Add basic registration page

--- bird_sound_quiz/urls.py
+++ bird_sound_quiz/urls.py
@@ -20,10 +20,14 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
+from accounts.views import register
+
+
 urlpatterns = [
     path("", include("quiz.urls")),
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("register/", register, name="register"),
     path('admin/', admin.site.urls),
 ]
 
