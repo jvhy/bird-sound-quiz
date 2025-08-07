11be56505949dc645493248507b54798e9bd554e
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-08-07 20:15:35 +0300

    Save results of finished quizzes to db

--- quiz/urls.py
+++ quiz/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("quiz/", views.quiz_page, name="quiz"),
     path("results/", views.results_page, name="results"),
+    path("quiz/results/<uuid:quiz_id>/", views.results_page_get, name="results_get"),
     path("check_answer/", views.check_answer, name="check_answer")
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -2,10 +2,11 @@ import random
 
 from django.conf import settings
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
+from django.utils import timezone
 
-from quiz.models import Recording
+from quiz.models import Recording, Quiz, Answer
 from quiz.services import get_available_regions, get_species_by_region, get_quiz_recordings, get_multiple_choices
 
 
@@ -18,16 +19,17 @@ def index(request):
 def quiz_page(request):
     region_id = request.POST.get("region")
     mode = request.POST.get("mode")
+    started_at = timezone.now().astimezone(timezone.get_default_timezone()).isoformat()
     region_species = get_species_by_region(region_id)
     quiz_species = random.sample(list(region_species), 10)
     recordings = get_quiz_recordings(quiz_species)
     options = {}
     match mode:
-        case "choice":
+        case "MULTI":
             for sp in quiz_species:
                 sp_options = get_multiple_choices(sp, region_species, 3, "random")
                 options[sp.id] = sp_options
-        case "open":
+        case "OPEN":
             pass
         case _:
             raise ValueError("Unexpected game mode")
@@ -39,7 +41,9 @@ def quiz_page(request):
             "recordings": recordings,
             "audio_field": audio_field,
             "options": options,
-            "mode": mode
+            "mode": mode,
+            "region_id": region_id,
+            "started_at": started_at
         }
     )
 
@@ -59,16 +63,40 @@ def check_answer(request):
 
 @require_http_methods(["POST"])
 def results_page(request):
+    quiz = Quiz(
+        user=request.user,
+        mode=request.POST.get("mode"),
+        started_at=request.POST.get("started_at")
+    )
+    quiz.region_id = request.POST.get("region_id")
+    quiz.save()
+
     recording_ids = request.POST.getlist("ids[]")
     user_answers = []
     for i in range(10):
-        answer = request.POST.get(f"answer_{i}")
-        answer = answer.capitalize() if answer else "<no answer>"
+        user_answer = request.POST.get(f"answer_{i}")
+        answer = Answer(
+            quiz=quiz,
+            user_answer=user_answer or ""
+        )
+        answer.recording_id = recording_ids[i]
         user_answers.append(answer)
-    audio_urls = request.POST.getlist("audio_urls[]")
-    recordings = Recording.objects.filter(id__in=recording_ids).select_related("species")
-    recordings_by_id = {str(r.id): r for r in recordings}
-    correct_answers = [recordings_by_id[rec_id].species.name_en.capitalize() for rec_id in recording_ids]
-    results = list(zip(user_answers, correct_answers, audio_urls))
-    score = sum(ua == ca for ua, ca, _ in results)
-    return render(request, 'results.html', context={"score": score, "results": results})
+    Answer.objects.bulk_create(user_answers)
+
+    return redirect("results_get", quiz_id=quiz.id)
+
+
+@require_http_methods(["GET"])
+def results_page_get(request, quiz_id):
+    quiz = Quiz.objects.select_related("user").get(id=quiz_id)
+    answers = Answer.objects.filter(quiz=quiz).order_by("id").select_related("recording__species")
+    results = [
+        (
+            ans.user_answer.capitalize() or "<no answer>",
+            ans.recording.species.name_en.capitalize(),
+            ans.recording.audio.url if settings.SELF_HOST_AUDIO else ans.recording.xc_audio_url
+        )
+        for ans in answers
+    ]
+
+    return render(request, "results.html", context={"score": quiz.score, "results": results})
