c0b6430d291f02a553cce2476808846e1d692c0a
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-08-12 10:52:11 +0300

    Add English locale files for base templates

--- bird_sound_quiz/settings.py
+++ bird_sound_quiz/settings.py
@@ -130,7 +130,7 @@ LOGIN_ENABLED = env("LOGIN_ENABLED", default=True)
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
@@ -138,6 +138,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [
+    BASE_DIR / "locale"
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
