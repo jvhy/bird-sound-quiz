92a3b3fa9ea8da2d7dc8c41eca69bb64f14cae56
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-08-13 19:56:56 +0300

    Add language selection widget

--- bird_sound_quiz/urls.py
+++ bird_sound_quiz/urls.py
@@ -23,6 +23,7 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("quiz.urls")),
     path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 if settings.LOGIN_ENABLED:
