06d3d093bf7a4406ea2d131d0761e9fd36367eb1
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-08-14 09:39:56 +0300

    Add Regions to admin view

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from quiz.models import Region
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('code', 'display_name', 'parent_region')
+    search_fields = ('code', 'name_en', 'name_fi')
+    list_filter = ('parent_region',)
+    ordering = ('code',)
+
+    # Optional: make display_name sortable by code
+    def display_name(self, obj):
+        return obj.display_name
+
+    display_name.admin_order_field = 'name_en'
+    display_name.short_description = 'Display Name'
