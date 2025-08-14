12091ea45690a811fe561ae74817b093bd3ee458
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-08-14 21:14:24 +0300

    Add unit tests for retrieving species by region

--- quiz/tests/test_services.py
+++ quiz/tests/test_services.py
@@ -3,7 +3,7 @@
 from model_bakery import baker
 import pytest
 
-from quiz.models import Region, Species, Observation
+from quiz.models import Region, Species, Observation, Recording
 from quiz import services
 
 
@@ -69,3 +69,95 @@ def test_get_available_regions_2():
 
     assert len(available_regions) == 3
     assert [region.name_fi for region in available_regions] == ["A-Alue", "B-Alue", "C-Alue"]  # ordered by Finnish name, as specified by locale arg
+
+
+@pytest.mark.django_db
+def test_get_species_by_region_1():
+    """Species for a given region should only include species that have been observed in that region."""
+    region = baker.make(Region)
+
+    species_1 = baker.make(Species, name_en="Blue-footed Booby")
+    species_2 = baker.make(Species, name_en="Tufted Titmouse")
+    species_3 = baker.make(Species, name_en="Andean Cock-of-the-rock")
+
+    baker.make(Observation, region=region, species=species_1)
+    baker.make(Observation, region=region, species=species_2)
+    # species 3 has no observations in the region
+
+    # all species have a recording
+    baker.make(Recording, species=species_1, _create_files=True)
+    baker.make(Recording, species=species_2, _create_files=True)
+    baker.make(Recording, species=species_3, _create_files=True)
+
+    region_species = services.get_species_by_region(region.id)
+
+    assert {species.name_en for species in region_species} == {"Blue-footed Booby", "Tufted Titmouse"}
+
+
+@pytest.mark.django_db
+def test_get_species_by_region_2():
+    """Species for a given region should only include species that have at least one recording."""
+    region = baker.make(Region)
+
+    species_1 = baker.make(Species, name_en="Blue-footed Booby")
+    species_2 = baker.make(Species, name_en="Tufted Titmouse")
+    species_3 = baker.make(Species, name_en="Andean Cock-of-the-rock")
+
+    # all species have observations in region
+    baker.make(Observation, region=region, species=species_1)
+    baker.make(Observation, region=region, species=species_2)
+    baker.make(Observation, region=region, species=species_3)
+
+    baker.make(Recording, species=species_1, _create_files=True)
+    # no recording for species 2
+    baker.make(Recording, species=species_3, _create_files=True)
+
+    region_species = services.get_species_by_region(region.id)
+
+    assert {species.name_en for species in region_species} == {"Blue-footed Booby", "Andean Cock-of-the-rock"}
+
+
+@pytest.mark.django_db
+def test_get_species_by_region_3():
+    """Species for a given region should only be listed once, i.e. retuned species objects should be distinct."""
+    region = baker.make(Region)
+
+    species_1 = baker.make(Species, name_en="Blue-footed Booby")
+    species_2 = baker.make(Species, name_en="Tufted Titmouse")
+    species_3 = baker.make(Species, name_en="Andean Cock-of-the-rock")
+
+    baker.make(Observation, region=region, species=species_1)
+    baker.make(Observation, region=region, species=species_2)
+    baker.make(Observation, region=region, species=species_3)
+
+    baker.make(Recording, species=species_1, _create_files=True)
+    baker.make(Recording, species=species_1, _create_files=True)  # species 1 has two recordings
+    baker.make(Recording, species=species_2, _create_files=True)
+    baker.make(Recording, species=species_3, _create_files=True)
+
+    region_species = services.get_species_by_region(region.id)
+
+    assert len(region_species) == 3
+
+
+@pytest.mark.django_db
+def test_get_species_by_region_4():
+    """Species observed outside the specified region shouldn't show up in results."""
+    target_region = baker.make(Region)
+    irrelevant_region = baker.make(Region)
+
+    species_1 = baker.make(Species, name_en="Blue-footed Booby")
+    species_2 = baker.make(Species, name_en="Tufted Titmouse")
+    species_3 = baker.make(Species, name_en="Andean Cock-of-the-rock")
+
+    baker.make(Observation, region=irrelevant_region, species=species_1)  # shouldn't show up when querying species from target_region
+    baker.make(Observation, region=target_region, species=species_2)
+    baker.make(Observation, region=target_region, species=species_3)
+
+    baker.make(Recording, species=species_1, _create_files=True)
+    baker.make(Recording, species=species_2, _create_files=True)
+    baker.make(Recording, species=species_3, _create_files=True)
+
+    region_species = services.get_species_by_region(target_region.id)
+
+    assert {species.name_en for species in region_species} == {"Tufted Titmouse", "Andean Cock-of-the-rock"}
